8347fdbb5e43dfdb94dda3cd772025da1cb6b71d
Ruchita Tapase <ruchitatapase74@gmail.com>
2026-08-23 16:36:52 +0530

    Add files via upload

---
+++ 01-python-basics/01-variables/variable_1.py
@@ -0,0 +1,64 @@
+# Python Variables
+
+# This file contains my practice examples for Python variables.
+
+# 1. Creating variables
+
+name = "Ruchita"
+age = 25
+height = 5.5
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# 2. Checking the type of a variable
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# 3. Assigning values to variables
+
+city = "Mumbai"
+country = "India"
+
+print(city)
+print(country)
+
+# 4. Changing the value of a variable
+
+score = 80
+print(score)
+
+score = 95
+print(score)
+
+# 5. Multiple variables
+
+first_name = "Ruchita"
+last_name = "Tapase"
+
+print(first_name, last_name)
+
+# 6. Variables can be used in calculations
+
+marks_1 = 80
+marks_2 = 90
+marks_3 = 85
+
+total_marks = marks_1 + marks_2 + marks_3
+average_marks = total_marks / 3
+
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+
+# 7. Using variables inside a sentence
+
+name = "Ruchita"
+age = 25
+
+print(f"My name is {name} and I am {age} years old.")
