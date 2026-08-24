9ea91f2ffe66165c454a2be7516821f2e4580611
Ruchita Tapase <ruchitatapase74@gmail.com>
2026-08-24 18:16:06 +0530

    Update variables.py

--- 01-python-basics/01-variables/variables.py
+++ 01-python-basics/01-variables/variables.py
@@ -1,1 +1,76 @@
+# Python Variables
+
+## What I Learned
+
+A variable is a name used to store a value in Python.
+
+Python variables can store different types of values such as:
+
+* String
+* Integer
+* Float
+* Boolean
+
+## Examples
+
+```python
+name = "Ruchita"
+age = 25
+height = 5.5
+is_student = True
+```
+
+##  Checking Variable Types
+
+Python provides the `type()` function to check the type of a variable.
+
+```python
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+```
+
+## Changing Variable Values
+
+A variable can be assigned a new value.
+
+```python
+score = 80
+score = 95
+```
+
+## ➕ Using Variables in Calculations
+
+Variables can be used to perform calculations.
+
+```python
+marks_1 = 80
+marks_2 = 90
+marks_3 = 85
+
+total_marks = marks_1 + marks_2 + marks_3
+average_marks = total_marks / 3
+```
+
+##  What I Practiced
+
+* Creating variables
+* Assigning values
+* Checking data types
+* Updating variable values
+* Using variables in calculations
+* Using variables inside strings
+
+##  Data Science Connection
+
+Variables are one of the basic building blocks of Python.
+
+In Data Science, variables are used to store:
+
+* Dataset values
+* Calculations
+* Statistics
+* Model parameters
+* Configuration values
 
