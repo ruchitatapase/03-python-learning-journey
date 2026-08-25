306e35058aab88c82382ee71b7b29e2b827ba3a5
Ruchita Tapase <ruchitatapase74@gmail.com>
2026-08-25 20:16:22 +0530

    Add files via upload

--- 01-python-basics/02-data-types/data_types.py
+++ 01-python-basics/02-data-types/data_types.py
@@ -1,1 +1,16 @@
+# Python Data Types
 
+name = "Ruchita"       # String
+age = 25               # Integer
+height = 5.4           # Float
+is_student = True      # Boolean
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
